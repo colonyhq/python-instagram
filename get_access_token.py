4942bc96c1498dab951536c71a9f70bdee62f0a7
Christopher Penkin <chris.penkin@gmail.com>
2016-07-13 23:33:59 +0200

    CNG-2246
    Fixed the access token call.

--- get_access_token.py
+++ get_access_token.py
@@ -31,13 +31,13 @@ if not scope or scope == [""]:
     scope = ["basic"]
 
 api = InstagramAPI(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
-redirect_uri = api.get_authorize_login_url(scope = scope)
+redirect_uri = api.get_authorize_login_url(scope=scope)
 
-print ("Visit this page and authorize access in your browser: "+ redirect_uri)
+print ("Visit this page and authorize access in your browser: " + redirect_uri)
 
 code = (str(input("Paste in code in query string after redirect: ").strip()))
 
 access_token = api.exchange_code_for_access_token(code)
-print ("access token: " )
+print ("access token: ")
 print (access_token)
 
