8b6bfcc069bae9498c9fabec1c7bca01a6f0342f
Jean Moggee <jmoggee@gmail.com>
2017-10-17 13:16:02 +0200

    Update models.py

--- instagram/models.py
+++ instagram/models.py
@@ -39,6 +39,13 @@ class Image(ApiModel):
 
 class Video(Image):
 
+    #2017-09-27: Video media type returns a new kwarg: id
+    #def __init__(self, url, width, height):
+    def __init__(self, *args, **kwargs):
+        for k,v in kwargs.iteritems():
+            setattr(self,k,v)
+        super(Image, self).__init__(*args)
+
     def __unicode__(self):
         return "Video: %s" % self.url
 
